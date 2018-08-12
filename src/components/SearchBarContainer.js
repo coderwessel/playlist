297de8b2e3bf463930266b7456bd7ae52f1fca54
coderwessel <nospam917@gmail.com>
2018-08-12 12:25:44 +0200

    downshift search with type ahead autocomplete added to searchbarcontainer

--- src/components/SearchBarContainer.js
+++ src/components/SearchBarContainer.js
@@ -2,18 +2,17 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux'
 import { withStyles } from '@material-ui/core/styles';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+// import Toolbar from '@material-ui/core/Toolbar';
+// import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
-
+// import keycode from 'keycode'
+import Downshift from 'downshift'
+import MenuItem from '@material-ui/core/MenuItem'
+import Paper from '@material-ui/core/Paper'
 
 const styles = theme =>({
-  container: {
-    display: 'flex',
-    flexWrap: 'wrap',
-  },
   textField: {
     marginLeft: theme.spacing.unit,
     marginRight: theme.spacing.unit,
@@ -23,33 +22,143 @@ const styles = theme =>({
     marginLeft: -12,
     marginRight: 20,
   },
+//from demo
+  root: {
+    flexGrow: 1,
+    height: 250,
+  },
+  container: {
+    flexGrow: 1,
+    position: 'relative',
+  },
+  paper: {
+    position: 'absolute',
+    zIndex: 1,
+    marginTop: theme.spacing.unit,
+    left: 0,
+    right: 0,
+  },
+  chip: {
+    margin: `${theme.spacing.unit / 2}px ${theme.spacing.unit / 4}px`,
+  },
+  inputRoot: {
+    flexWrap: 'wrap',
+  },
 })
+
+const suggestions = [
+  { label: 'toto - Rosanna' },
+  { label: 'Cure - Forest' },
+  { label: 'Prince - Kiss' },
+  { label: 'Metallica - Nothing else Matters' },
+  { label: 'David Bowie - Let\'s Dance' },
+  { label: 'Calvin Harris - Colors' }
+]
+
+function renderInput(inputProps) {
+  const { InputProps, classes, ref, ...other } = inputProps;
+
+  return (
+    <TextField
+      InputProps={{
+        inputRef: ref,
+        classes: {
+          root: classes.inputRoot,
+        },
+        ...InputProps,
+      }}
+      {...other}
+    />
+  );
+}
+
+function renderSuggestion({ suggestion, index, itemProps, highlightedIndex, selectedItem }) {
+  const isHighlighted = highlightedIndex === index;
+  const isSelected = (selectedItem || '').indexOf(suggestion.label) > -1;
+
+  return (
+    <MenuItem
+      {...itemProps}
+      key={suggestion.label}
+      selected={isHighlighted}
+      component="div"
+      style={{
+        fontWeight: isSelected ? 500 : 400,
+      }}
+    >
+      {suggestion.label}
+    </MenuItem>
+  );
+}
+
+renderSuggestion.propTypes = {
+  highlightedIndex: PropTypes.number,
+  index: PropTypes.number,
+  itemProps: PropTypes.object,
+  selectedItem: PropTypes.string,
+  suggestion: PropTypes.shape({ label: PropTypes.string }).isRequired,
+};
+
+function getSuggestions(inputValue) {
+  let count = 0;
+
+  return suggestions.filter(suggestion => {
+    const keep =
+      (!inputValue || suggestion.label.toLowerCase().indexOf(inputValue.toLowerCase()) !== -1) &&
+      count < 5;
+
+    if (keep) {
+      count += 1;
+    }
+
+    return keep;
+  });
+}
 
 class SearchBarContainer extends React.PureComponent {
   state = {  };
 
-  handleChange = (e) => {
-      this.setState({ [e.target.name]: e.target.value })
-    }
+  // TODO: how do I add the slection to the playlist??
+  // handleChange = (e) => {
+  //     this.setState({ [e.target.name]: e.target.value })
+  //   }
 
   render () {
     const { classes } = this.props;
     return (
-      <div>
-        <form className={classes.container} noValidate autoComplete="off">
-          <TextField
-            id="name"
-            label="Find Music"
-            className={classes.textField}
-            value={this.state.name}
-            onChange={this.handleChange}
-            margin="normal"
-          />
-          <IconButton className={classes.menuButton} color="inherit" aria-label="Search">
-            <SearchIcon />
-          </IconButton>
-        </form>
-</div>
+      <div className={classes.root}>
+      <Downshift>
+        {({ getInputProps, getItemProps, isOpen, inputValue, selectedItem, highlightedIndex }) => (
+          <div className={classes.container}>
+            {renderInput({
+              fullWidth: true,
+              classes,
+              InputProps: getInputProps({
+                placeholder: 'Search Music (start typing)',
+                id: 'integration-downshift-simple',
+              }),
+            })}
+            {/* TODO: I would like a searchicon here
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Search">
+             <SearchIcon />
+           </IconButton> */}
+            {isOpen ? (
+              <Paper className={classes.paper} square>
+                {getSuggestions(inputValue).map((suggestion, index) =>
+                  renderSuggestion({
+                    suggestion,
+                    index,
+                    itemProps: getItemProps({ item: suggestion.label }),
+                    highlightedIndex,
+                    selectedItem,
+                  }),
+                )}
+              </Paper>
+            ) : null}
+          </div>
+        )}
+      </Downshift>
+    </div>
     )
   }
 }
@@ -64,7 +173,6 @@ const mapStateToProps = (state) => {
     tracklist: state.tracklist
   }
 }
-
 
 // export default connect(null, { addAlbum })(AlbumsListContainer)
 export default withStyles(styles) (connect(mapStateToProps)(SearchBarContainer));
